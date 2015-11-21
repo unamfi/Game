39fb72c6479e19138a0991090884642dde0504ea
Julio Guzman <julio.guzman@me.com>
2015-11-21 13:57:34 -0600

    Remove unnecessary nodes from Game view controller

--- OSXGame/GameViewController.swift
+++ OSXGame/GameViewController.swift
@@ -17,32 +17,6 @@ class GameViewController: NSViewController {
         // create a new scene
         let scene = SCNScene(named: "art.scnassets/scene.scn")!
         
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = SCNLightTypeOmni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = SCNLightTypeAmbient
-        ambientLightNode.light!.color = NSColor.darkGrayColor()
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-//        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
-        
-
         // set the scene to the view
         self.gameView!.scene = scene
         
