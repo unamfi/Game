e2e4670e00a79ef5923b1d849ed6349e300fa5f0
Julio Guzman <julio.guzman@me.com>
2015-11-20 18:49:54 -0600

    Practica-1 Generación aleatoria de niveles

--- Game/GameViewController.swift
+++ Game/GameViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 import QuartzCore
 import SceneKit
 
+extension Array {
+    func randomItem() -> Element {
+        let index = Int(arc4random_uniform(UInt32(self.count)))
+        return self[index]
+    }
+}
+
 class GameViewController: UIViewController , UITextFieldDelegate {
 
     @IBOutlet var scnView: SCNView!
@@ -22,7 +29,7 @@ class GameViewController: UIViewController , UITextFieldDelegate {
         super.viewDidLoad()
         
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/scene.scn")!
+        let scene = SCNScene(named: "art.scnassets/emptyScene.scn")!
         
         // create and add a camera to the scene
         let cameraNode = SCNNode()
@@ -46,9 +53,6 @@ class GameViewController: UIViewController , UITextFieldDelegate {
         
         //Input
         self.textField.delegate = self
-        
-        //Player
-        self.smiley = self.scnView.scene?.rootNode.childNodeWithName("smiley", recursively: false)
         
     }
     
@@ -76,9 +80,30 @@ class GameViewController: UIViewController , UITextFieldDelegate {
     
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool{
         
-        self.smiley?.physicsBody?.applyForce(SCNVector3Make(0.0, 10, 0.0), impulse: true)
-        
+        if string == " " {
+            let scene2 = SCNScene(named: "art.scnassets/scene2.scn")!
+            let scene3 = SCNScene(named: "art.scnassets/scene3.scn")!
+            let scene4 = SCNScene(named: "art.scnassets/scene4.scn")!
+            self.addChildNodesFromTwoRandomScenesFromArrayToCurrentScene([ scene2, scene3, scene4 ])
+        }
+
         return false;
+    }
+    
+    func addChildNodesFromTwoRandomScenesFromArrayToCurrentScene(scenes:[SCNScene])
+    {
+        for childNode in (self.scnView.scene?.rootNode.childNodes)! {
+            childNode.removeFromParentNode()
+        }
+        
+        let firstScene = scenes.randomItem()
+        let secondScene = scenes.randomItem()
+        for node in firstScene.rootNode.childNodes {
+            self.scnView.scene!.rootNode.addChildNode(node)
+        }
+        for node in secondScene.rootNode.childNodes {
+            self.scnView.scene!.rootNode.addChildNode(node)
+        }
     }
 
 }
