9ace0b084fe23354016a80488153ecbd8aed61a2
Julio Guzman <julio.guzman@me.com>
2015-12-15 12:18:28 -0600

    Remove Practica scene renderer class

--- Game/Renderer.swift
+++ Game/Renderer.swift
@@ -9,24 +9,13 @@
 import Foundation
 import SceneKit
 
-class SceneRenderer: NSObject, SCNSceneRendererDelegate {
-    
+class SceneRenderer : NSObject, SCNSceneRendererDelegate {
     var scene : SCNScene
-    
-    init(scene: SCNScene) {
-        self.scene = scene
-    }
-}
-
-class PracticaSceneRenderer : SceneRenderer
-{
-    
     var lastTime : NSTimeInterval = NSTimeInterval()
     var currentTime : NSTimeInterval = NSTimeInterval()
     
-    override init(scene: SCNScene) {
-        super.init(scene: scene)
-
+    init(scene: SCNScene) {
+        self.scene = scene
     }
     
     func renderer(renderer: SCNSceneRenderer, updateAtTime time: NSTimeInterval) {
