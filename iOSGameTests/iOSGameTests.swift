a3e1b04b8e74de59d3211a6cc5218c7b8b0aadbf
Julio César Guzmán <julio.guzman@me.com>
2016-01-12 15:49:01 -0600

    Revert "Add failing test to test Travis CI build failure"
    This reverts commit f2e3f55583e92e141426d87e2308413bd4f408fb.

--- iOSGameTests/iOSGameTests.swift
+++ iOSGameTests/iOSGameTests.swift
@@ -26,10 +26,6 @@ class iOSGameTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-    func testFail() {
-        XCTAssert(false)
-    }
-    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
